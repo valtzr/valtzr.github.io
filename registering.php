9b7e939c64617561e9f39195cb88292353e5a1af
valtzr <74674772+valtzr@users.noreply.github.com>
2026-04-06 05:45:16 +0700

    Update registering.php

--- registering.php
+++ registering.php
@@ -1,15 +1,6 @@
 <?php
 
-$link = mysqli_connect("localhost", "root", "root");
+$username = $_POST["username"];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $phonenumber = $_POST['phonenumber'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    
-    $sql = "INSERT INTO users (username, phonenumber, email, password) VALUES (?, ?, ?, ?)";
-   
-}
+echo $username;
 ?>
